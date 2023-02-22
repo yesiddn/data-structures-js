823e876af8773cebad34319612615b9fc6b2019d
Yesid <yesidrodriguez305@gmail.com>
2023-02-21 22:20:24 -0500

    Implement Set method to HashTable class

--- hash-tables/hash-tables.js
+++ hash-tables/hash-tables.js
@@ -11,6 +11,28 @@ class HashTable {
     }
     return hash;
   }
+
+  set(key, value) {
+    const address = this.hashMethod(key);
+    if (!this.data[address]) {
+      this.data[address] = [];
+    }
+    this.data[address].push([key, value]);
+    return this.data;
+    /* 
+    [ 
+      [ 
+        [ 'grapes', 10000 ] 
+      ], 
+      [ 
+        [ 'apples', 9 ] , 
+        [ 'oranges', 10 ] 
+      ] 
+    ]
+    */
+  }
 }
 
 const myFirstHashTable = new HashTable(50); // 50 buckets
+
+myFirstHashTable.set('Duvan', 2004); // (50) [empty × 34, Array(1), empty × 15] en la posición 34 se encuentra el array con el key y el value que se le asignó
