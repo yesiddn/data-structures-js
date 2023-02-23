69beaa0baa3d3e2ba9c695e0580d49632b058bc4
Yesid <yesidrodriguez305@gmail.com>
2023-02-22 22:04:55 -0500

    Implement Get method to HashTable class

--- hash-tables/hash-tables.js
+++ hash-tables/hash-tables.js
@@ -12,6 +12,22 @@ class HashTable {
     return hash;
   }
 
+  // Get method
+  get(key) {
+    const address = this.hashMethod(key);
+    const currentBucket = this.data[address];
+
+    if (currentBucket) {
+      for (let i = 0; i < currentBucket.length; i++) {
+        if (currentBucket[i][0] === key) {
+          return currentBucket[i][1];
+        }
+      }
+    }
+    return undefined;
+  }
+
+  // Set method
   set(key, value) {
     const address = this.hashMethod(key);
     if (!this.data[address]) {
@@ -36,3 +52,6 @@ class HashTable {
 const myFirstHashTable = new HashTable(50); // 50 buckets
 
 myFirstHashTable.set('Duvan', 2004); // (50) [empty × 34, Array(1), empty × 15] en la posición 34 se encuentra el array con el key y el value que se le asignó
+myFirstHashTable.set('Ana', 2000);
+
+myFirstHashTable.get('Duvan'); // 2004
