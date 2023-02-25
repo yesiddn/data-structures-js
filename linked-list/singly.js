7b398f68ac5e7c39ae4fa0e2e95e73c76283f78b
Yesid <yesidrodriguez305@gmail.com>
2023-02-25 10:30:12 -0500

    Add Singly Linked List object

---
+++ linked-list/singly.js
@@ -0,0 +1,18 @@
+let singlyLinkedList = {
+  head: {
+    value: 1,
+    next: {
+      value: 2,
+      next: {
+        value: 3,
+        next: {
+          value: 4,
+          next: {
+            value: 5,
+            next: null
+          }
+        }
+      }
+    }
+  }
+};
