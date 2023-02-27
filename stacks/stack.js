d30f870c8c0796612bca788ab073af3bc1dd998e
Yesid <yesidrodriguez305@gmail.com>
2023-02-26 21:18:38 -0500

    Implement the Peek method to the Stack class

--- stacks/stack.js
+++ stacks/stack.js
@@ -1,7 +1,18 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
 class Stack {
   constructor() {
     this.top = null;
     this.bottom = null;
     this.length = 0;
+  }
+
+  peek() {
+    return this.top;
   }
 }
