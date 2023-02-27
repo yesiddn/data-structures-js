d0e000d18d68418c61ad53b7a401fdb8856200f9
Yesid <yesidrodriguez305@gmail.com>
2023-02-26 21:22:18 -0500

    Implement the Push method to the Stack class

--- stacks/stack.js
+++ stacks/stack.js
@@ -15,4 +15,22 @@ class Stack {
   peek() {
     return this.top;
   }
+
+  push(value) {
+    const newNode = new Node(value);
+    if (this.length === 0) {
+      this.top = newNode;
+      this.bottom = newNode;
+    } else {
+      const holdingPointer = this.top; // se guarda el valor de top en holdingPointer para no perderlo por el garbage collector
+      this.top = newNode; // se asigna el nuevo nodo a top
+      this.top.next = holdingPointer; // se asigna el valor de holdingPointer a next de top
+    }
+
+    this.length++;
+
+    return this;
+  }
+
+  
 }
