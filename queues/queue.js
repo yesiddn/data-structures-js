fbd253d41896de797c138e47166ad28b2c87e017
Yesid <yesidrodriguez305@gmail.com>
2023-02-28 21:35:25 -0500

    Implement the Enqueue method to the Queue class

--- queues/queue.js
+++ queues/queue.js
@@ -15,6 +15,21 @@ class Queue {
   peek() {
     return this.first;
   }
+
+  enqueue(value) {
+    const newNode = new Node(value);
+
+    if (this.length === 0) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+
+    this.length++;
+    return this;
+  }
 }
 
 const myQueue = new Queue();
