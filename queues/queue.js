bdb26ab06132066121ca0b7adce75d1af161d041
Yesid <yesidrodriguez305@gmail.com>
2023-02-28 21:45:52 -0500

    Implement the Dequeue method to the Queue class

--- queues/queue.js
+++ queues/queue.js
@@ -16,6 +16,8 @@ class Queue {
     return this.first;
   }
 
+  // enqueue method
+  // add a element to the end of the queue
   enqueue(value) {
     const newNode = new Node(value);
 
@@ -30,6 +32,28 @@ class Queue {
     this.length++;
     return this;
   }
+
+  // dequeue method
+  // remove a element from the beginning of the queue
+  dequeue() {
+    if (!this.first) {
+      return 'Queue is empty';
+    } else if (this.first === this.last) {
+      this.first = null;
+      this.last = null;
+    } else {
+      this.first = this.first.next;
+    }
+
+    this.length--;
+    return this;
+  }
 }
 
 const myQueue = new Queue();
+
+myQueue.enqueue(1);
+myQueue.enqueue(2);
+myQueue.enqueue(3);
+
+myQueue.dequeue();
