07f6c371a4ae58026654d673849b64960ce93b39
Yesid <yesidrodriguez305@gmail.com>
2023-03-01 22:08:54 -0500

    Add BinarySearchTree class without methods

---
+++ trees/tree.js
@@ -0,0 +1,17 @@
+//    10
+//  4     20
+// 2 8  17  120
+
+class Node {
+  constructor(value) {
+    this.left = null;
+    this.right = null;
+    this.value = value;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+}
