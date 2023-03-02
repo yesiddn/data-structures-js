d9fbe2d304fff644f813997ce53abd995143e131
Yesid <yesidrodriguez305@gmail.com>
2023-03-02 16:34:02 -0500

    Implement the insert method to the BinarySearchTree class

--- trees/tree.js
+++ trees/tree.js
@@ -14,4 +14,33 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
+
+  // Insert a node
+  insert(value) {
+    const newNode = new Node(value);
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    } else {
+      let currentNode = this.root;
+      
+      while (true) {
+        if (value < currentNode.value) {
+          if (!currentNode.left) {
+            currentNode.left = newNode;
+            return this;            
+          }
+          currentNode = currentNode.left;
+        } else {
+          if (!currentNode.right) {
+            currentNode.right = newNode;
+            return this;            
+          }
+          currentNode = currentNode.right;
+        }
+      }
+    }
+  }
 }
+
+const tree = new BinarySearchTree();
