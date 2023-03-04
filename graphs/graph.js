a9708d272ec7de228a015ddc8399b0ccf595da83
Yesid <yesidrodriguez305@gmail.com>
2023-03-03 23:41:06 -0500

    Add representation of a Graph with Adjacency List

--- graphs/graph.js
+++ graphs/graph.js
@@ -12,3 +12,5 @@ const graph = [
   [1, 3],
 ]; // grafo hardcodeado
 
+// Adjacency list: representa los nodos y sus conexiones
+const anotherGraph = [[2], [2, 3], [0, 1, 3], [1, 2]]; // el indice 0 conecta con el 2, el 1 conecta con el 2 y el 3, etc
