63ef7c9de4eef37c068cda2ddceaf861592d3241
Yesid <yesidrodriguez305@gmail.com>
2023-03-04 16:30:10 -0500

    Implement the AddEdge method to the Graph class

--- graphs/graph.js
+++ graphs/graph.js
@@ -1,3 +1,5 @@
+// Graph type: Undirected graph with adjacent list
+
 class Graph {
   constructor() {
     this.nodes = 0;
@@ -11,6 +13,13 @@ class Graph {
 
     return this;
   }
+  
+  addEdge(nodeOne, nodeTwo) {
+    this.adjacentList[nodeOne].push(nodeTwo);
+    this.adjacentList[nodeTwo].push(nodeOne);
+
+    return this;
+  }
 }
 
 const myGraph = new Graph();
@@ -20,3 +29,11 @@ myGraph.addVertex('4');
 myGraph.addVertex('5');
 myGraph.addVertex('6');
 myGraph.addVertex('8');
+
+myGraph.addEdge('1', '3');
+myGraph.addEdge('1', '4');
+myGraph.addEdge('1', '6');
+myGraph.addEdge('3', '5');
+myGraph.addEdge('3', '6');
+myGraph.addEdge('4', '5');
+myGraph.addEdge('4', '8');
