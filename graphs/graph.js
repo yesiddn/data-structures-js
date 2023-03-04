d4a64375e98bf763692672559b9268b07897f46a
Yesid <yesidrodriguez305@gmail.com>
2023-03-04 16:03:40 -0500

    Implement the AddVertex method to the Graph class

--- graphs/graph.js
+++ graphs/graph.js
@@ -3,4 +3,20 @@ class Graph {
     this.nodes = 0;
     this.adjacentList = {};
   }
+
+  // addVertex method: agrega un nodo al grafo
+  addVertex(node) {
+    this.adjacentList[node] = [];
+    this.nodes++;
+
+    return this;
+  }
 }
+
+const myGraph = new Graph();
+myGraph.addVertex('1');
+myGraph.addVertex('3');
+myGraph.addVertex('4');
+myGraph.addVertex('5');
+myGraph.addVertex('6');
+myGraph.addVertex('8');
