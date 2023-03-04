58a4cd99d932b0bcd89a70d1cd310e145095358e
Yesid <yesidrodriguez305@gmail.com>
2023-03-03 23:45:28 -0500

    Add representation of a Graph with Adjacent List using hash tables

--- graphs/graph.js
+++ graphs/graph.js
@@ -14,3 +14,11 @@ const graph = [
 
 // Adjacency list: representa los nodos y sus conexiones
 const anotherGraph = [[2], [2, 3], [0, 1, 3], [1, 2]]; // el indice 0 conecta con el 2, el 1 conecta con el 2 y el 3, etc
+
+// Adjacency matrix (hash table): representa los nodos y sus conexiones
+const graphTable = {
+  0: [2],
+  1: [2, 3],
+  2: [0, 1, 3],
+  3: [1, 2],
+};
